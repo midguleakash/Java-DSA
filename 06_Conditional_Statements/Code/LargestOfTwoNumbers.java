36759c66b7317ef641b9e152ed5af7f873b49d1b
midguleakash <midguleakash@gmail.com>
2026-07-07 21:00:22 +0530

    Add Conditional Statements

---
+++ 06_Conditional_Statements/Code/LargestOfTwoNumbers.java
@@ -0,0 +1,15 @@
+public class LargestOfTwoNumbers {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        if(a > b){
+            System.out.println("a is larger");
+        }
+
+        else {
+            System.out.println("b is larger");
+        }
+    }
+}
