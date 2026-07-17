a058daf2cacf6a7a09c6c7651356773c8daeee33
midguleakash <midguleakash@gmail.com>
2026-07-17 23:41:19 +0530

    print number from 1 to 10 using while loop

---
+++ 07_Loops/Code/PrintNumbers1To10.java
@@ -0,0 +1,12 @@
+// using while loop
+
+public class PrintNumbers1To10 {
+    public static void main(String[] args){
+        int i = 1;
+
+        while(i <= 10){
+            System.out.println(i);
+            i++ ;
+        }
+    }
+}
