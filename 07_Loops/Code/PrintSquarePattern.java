90a7c8add9d46c331d7ed4c383b2ed74890506d0
midguleakash <midguleakash@gmail.com>
2026-07-20 22:01:15 +0530

    print sqaure pattern using for loop

---
+++ 07_Loops/Code/PrintSquarePattern.java
@@ -0,0 +1,15 @@
+// print sqaure pattern using loop
+
+import java.util.* ;
+
+public class PrintSquarePattern {
+    public static void main(String[] args){
+        int n=5 ;
+        for(int i=1 ; i <=n; i++){
+            for(int j=1 ; j <= n; j++){
+                System.out.print("* ")
+            }
+            System.out.println();
+        }
+    }
+}
