464e1a942e71253fed71e5080384f64c839ae461
midguleakash <midguleakash@gmail.com>
2026-07-21 21:30:43 +0530

    Solve code print reverse of number

---
+++ 07_Loops/Code/ReverseANumber.java
@@ -0,0 +1,12 @@
+public class ReverseANumber {
+    public static void main(String[] args){
+        int n = 123654;
+        int digit ;
+
+        while(n > 0) {
+            digit = n % 10 ;
+            System.out.print(digit);
+            n = n / 10 ;
+        }
+    }
+}
