5a113dec49bd128f066af6c59ad2e518d3565ff1
midguleakash <midguleakash@gmail.com>
2026-07-23 10:19:17 +0530

    print 1 to 10 number using do while loop

---
+++ 07_Loops/Code/DoWhile.java
@@ -0,0 +1,13 @@
+// print 1 to 10 number using do while loop
+
+public class DoWhile {
+    public static void main(String[] args){
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++ ;
+        }
+        while(i <= 10);
+    }
+}
