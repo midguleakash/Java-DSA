2d64d4ca9b183a23d0411c8f19c9b53f43f710ec
midguleakash <midguleakash@gmail.com>
2026-07-23 10:22:40 +0530

    breka or stop loop using break keyword

---
+++ 07_Loops/Code/Break.java
@@ -0,0 +1,12 @@
+// breka or stop loop using break keyword
+
+public class Break {
+    public static void main(String[] args){
+        for(int i=1 ; i < 25; i++) {
+            System.out.println(i);
+            if(i == 11){
+                break ;
+            }
+        }
+    }
+}
