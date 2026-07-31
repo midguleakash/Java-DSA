ccb8e4ef77c5057782538adecdc2bc6ee57f1e4a
midguleakash <midguleakash@gmail.com>
2026-07-31 20:56:43 +0530

    add more function to implement linkedlist

--- 24_LinkedList/MySinglyLinkedList/MainApp.java
+++ 24_LinkedList/MySinglyLinkedList/MainApp.java
@@ -11,8 +11,14 @@ public class MainApp {
 		
 		
 		do {
-			System.out.println("\t1.Add at start");
-			System.out.println("\t2.Display a list");
+			System.out.println("\n\t1.Add at start");
+			System.out.println("\t2.Add at end");
+			System.out.println("\t3.Add at index");			
+			System.out.println("\t4.Delete at start");
+			System.out.println("\t5.Delete at end");
+			System.out.println("\t6.Delete at index");
+			System.out.println("\t7.Display a list");
+			System.out.println("\t8.Search data in a list");
 			System.out.println("\t10.Exit");
 			System.out.print("Enter choice : ");
 			choice = sc.nextInt();
@@ -28,7 +34,60 @@ public class MainApp {
 				}
 				
 				case 2 : {
+					int data ;					
+					System.out.print("Enter data : ");
+					data = sc.nextInt();
+					myLL.insertAtEnd(data);
+					break ;
+				}
+				
+				case 3 : {
+					int index ;					
+					System.out.print("Enter index : ");
+					index = sc.nextInt();
+					int data ;					
+					System.out.print("Enter data : ");
+					data = sc.nextInt();
+					myLL.insertAtPosition(data, index);
+					break ;
+				}
+				
+				case 4 : {
+					myLL.deleteAtBeg();
+					break ;
+				}
+				
+				case 5 : {
+					myLL.deleteAtEnd();
+					break ;
+				}
+				
+				case 6 : {
+					int index ;					
+					System.out.print("Enter index : ");
+					index = sc.nextInt();
+					myLL.deleteAtPosition(index);
+					break ;
+				}
+				
+				case 7 : {
 					myLL.display();
+					break ;
+				}
+				
+				case 8 : {
+					int data ;					
+					System.out.print("Enter data for search : ");
+					data = sc.nextInt();
+					if(myLL.search(data) == -1){
+						System.out.println("data not found");	
+						System.out.println("index of your data : " + -1);
+					}
+					else {
+						System.out.println("Data found");
+						System.out.println("index of your data : " + myLL.search(data));
+					}
+					
 					break ;
 				}
 				
--- 24_LinkedList/MySinglyLinkedList/MySinglyLinkedList.java
+++ 24_LinkedList/MySinglyLinkedList/MySinglyLinkedList.java
@@ -22,6 +22,139 @@ public class MySinglyLinkedList {
 		System.out.print("--- Node inserted at begining ---\n");		
 	}
 	
+	
+	public void insertAtPosition(int element, int position) {
+		Node newNode = new Node(element);
+		
+		if(position <= 0) {
+			System.out.println("Index strat from 1 ");
+			System.out.println("--- Invalid Index ----");
+			return ;
+		}
+		
+		if(position == 1) {
+			insertAtBeg(element);
+			return ;
+		}
+		
+		Node itr =start ;
+		int i = 1 ;
+		
+		while(i < position - 1  && itr != null) {
+			itr = itr.next ;
+			i++ ;
+		}
+		
+		if(itr == null) {
+			System.out.println("--- invalid index ---")	;
+			return ;
+		}
+		newNode.next = itr.next ;
+		itr.next = newNode ;
+		
+		System.out.print("--- Node inserted at given index ---\n");
+	}
+	
+	
+	public void insertAtEnd(int element) {
+		Node newNode = new Node(element);
+		
+		if(start == null) {
+			start = newNode ;
+			System.out.print("--- Node inserted at end ---\n");
+			return ;
+		}
+		
+		Node itr = start ;
+		while(itr.next != null) {
+			itr = itr.next ;
+		}
+		
+		itr.next = newNode ;
+		System.out.print("--- Node inserted at end ---\n");
+	}
+	
+	
+	
+	public void deleteAtBeg() {
+		if(start == null) {
+			System.out.println("--- List is empty ---\n");	
+			return ;
+		}		
+		start = start.next ;
+		System.out.println("--- delete start Node ---");
+	}
+	
+	
+	public void deleteAtEnd() {
+		if(start == null) {
+			System.out.println("--- List is empty ---\n");	
+			return ;
+		}
+		
+		if(start.next == null) {
+			start = null ;
+			System.out.println("--- Last node deleted ---");
+			return ;
+		}
+		
+		Node itr = start ;
+		while(itr.next.next != null ) {
+			itr = itr.next ;
+		}
+		itr.next = itr.next.next ;
+		System.out.println("--- delete start Node ---");
+	}
+	
+	
+	public void deleteAtPosition(int index) {
+		if(start == null) {
+			System.out.println("--- List is empty ---\n");	
+			return ;
+		}
+		
+		if(index == 1) {
+			deleteAtBeg();
+			return ;
+		}
+		
+		Node itr = start;
+		int i=1 ;
+		while(i < index - 1 && itr.next != null){
+			itr = itr.next ;
+			i++ ;
+		}
+		
+		if(itr.next == null) {
+			System.out.println("--- invalid index ---")	;
+			return ;
+		}
+		
+		itr.next = itr.next.next ;
+		System.out.println("--- delete this index Node ---");
+	}
+	
+	public int search(int data) {
+		
+		if(start == null) {
+			System.out.println("\n--- List is empty ---");	
+			return -1;
+		}
+		
+		int index = 1 ;
+		Node itr = start ;
+		
+		while(itr != null) {
+			if(data == itr.data) {
+				return index ;
+			}
+			
+			itr = itr.next ;
+			index ++ ;
+		}		
+		return -1;
+	}
+	
 	public void display() {
 		if(start == null) {
 			System.out.println("--- List is empty ---\n");			
